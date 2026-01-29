5b9517fd580b8960c7c8f497532975a30bfb2c1e
Gold star <93335223+Paneerty@users.noreply.github.com>
2026-01-29 17:07:41 +0100

    Add function to send WhatsApp message

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("الموقع متصل بصفحة فيسبوك بنجاح ✅");
+function sendWhatsApp() {
+  const message =
+    "Nouvelle réservation GS 4C\n" +
+    "Nom:\nTéléphone:\nService:\nDate:\nDétails:";
+
+  window.open(
+    "https://wa.me/21655138035?text=" + encodeURIComponent(message),
+    "_blank"
+  );
+}
